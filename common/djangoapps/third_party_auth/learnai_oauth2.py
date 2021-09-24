2c7c1a605fa730f3a16104b3d1db042d3b6e9093
titlethanason <titlethanason@gmail.com>
2021-09-24 18:19:56 +0700

    :pencil2: (learn-ai/oauth2) remove comment

--- common/djangoapps/third_party_auth/learnai_oauth2.py
+++ common/djangoapps/third_party_auth/learnai_oauth2.py
@@ -9,11 +9,9 @@ class LearnAIOAuth2(BaseOAuth2):
     STATE_PARAMETER = 1
 
     def get_user_details(self, response):
-        """Return user details from GitHub account"""
         return {'username': response.get('username')}
 
     def user_data(self, access_token, *args, **kwargs):
-        """Loads user data from service"""
         url = 'http://ai4all.t.innosoft.kmutt.ac.th/api/user/v1/me'
         header = {"Authorization": "Bearer %s" % access_token}
         return self.get_json(url, headers=header)
