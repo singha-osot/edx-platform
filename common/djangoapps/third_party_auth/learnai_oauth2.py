9b0f3a30b5c36913773b84a14fe8ee251a2e13ee
titlethanason <titlethanason@gmail.com>
2021-09-27 18:05:24 +0700

    :beetle: (learn-ai/oauth2) fix user_data return type

--- common/djangoapps/third_party_auth/learnai_oauth2.py
+++ common/djangoapps/third_party_auth/learnai_oauth2.py
@@ -9,12 +9,12 @@ class LearnAIOAuth2(BaseOAuth2):
     STATE_PARAMETER = 1
 
     def get_user_details(self, response):
-        return {'username': response[0].get('username'),
-                'fullname': response[0].get('name'),
-                'email': response[0].get('email'),
-                'profile_image': response[0].get('profile_image')}
+        return {'username': response.get('username'),
+                'fullname': response.get('name'),
+                'email': response.get('email'),
+                'profile_image': response.get('profile_image')}
 
     def user_data(self, access_token, *args, **kwargs):
         url = 'http://ai4all.t.innosoft.kmutt.ac.th/api/user/v1/accounts'
         header = {"Authorization": "Bearer %s" % access_token}
-        return self.get_json(url, headers=header)
+        return self.get_json(url, headers=header)[0]
