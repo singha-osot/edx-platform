83f81530713a8413d26a6bd06d2927235981b744
titlethanason <titlethanason@gmail.com>
2021-09-27 15:15:23 +0700

    :sparkles: (learn-ai/oauth2) add more user_details

--- common/djangoapps/third_party_auth/learnai_oauth2.py
+++ common/djangoapps/third_party_auth/learnai_oauth2.py
@@ -9,9 +9,12 @@ class LearnAIOAuth2(BaseOAuth2):
     STATE_PARAMETER = 1
 
     def get_user_details(self, response):
-        return {'username': response.get('username')}
+        return {'username': response[0].get('username'),
+                'fullname': response[0].get('name'),
+                'email': response[0].get('email'),
+                'profile_image': response[0].get('profile_image')}
 
     def user_data(self, access_token, *args, **kwargs):
-        url = 'http://ai4all.t.innosoft.kmutt.ac.th/api/user/v1/me'
+        url = 'http://ai4all.t.innosoft.kmutt.ac.th/api/user/accounts'
         header = {"Authorization": "Bearer %s" % access_token}
         return self.get_json(url, headers=header)
