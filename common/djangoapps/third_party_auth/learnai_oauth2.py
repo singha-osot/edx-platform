45ed30002669acf204d86238302b33f2961ab3c1
titlethanason <titlethanason@gmail.com>
2021-09-28 14:08:35 +0700

    :beetle: (learn-ai/oauth2) return username as user_id

--- common/djangoapps/third_party_auth/learnai_oauth2.py
+++ common/djangoapps/third_party_auth/learnai_oauth2.py
@@ -3,6 +3,7 @@ from social_core.backends.oauth import BaseOAuth2
 
 class LearnAIOAuth2(BaseOAuth2):
     name = 'learn-ai'
+    ID_KEY = 'username'
     AUTHORIZATION_URL = 'http://ai4all.t.innosoft.kmutt.ac.th/oauth2/authorize'
     ACCESS_TOKEN_URL = 'http://ai4all.t.innosoft.kmutt.ac.th/oauth2/access_token'
     ACCESS_TOKEN_METHOD = 'POST'
@@ -18,3 +19,11 @@ class LearnAIOAuth2(BaseOAuth2):
         url = 'http://ai4all.t.innosoft.kmutt.ac.th/api/user/v1/accounts'
         header = {"Authorization": "Bearer %s" % access_token}
         return self.get_json(url, headers=header)[0]
+
+    def get_user_id(self, details, response):
+        """Return a username prepared in get_user_details as uid"""
+        try:
+            user_id = details.get(self.ID_KEY)
+        except KeyError:
+            user_id = None
+        return user_id
